f1b0585bd3b38e30b0d55fe9971fa7108e8211b9
Emmanuel <Emmanuel.okum@idea2solutions.com>
2023-08-30 11:25:06 +0100

    refactor the navigation element

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,10 +8,8 @@
   
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-	<link href="toastr.css" rel="stylesheet"/>
-	<script src="https://code.jquery.com/jquery-3.6.3.min.js" integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
-	@vite('resources/js/app.js')
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/navigation.css') }}">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css" rel="stylesheet">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
 
@@ -33,7 +31,7 @@
             color:white;
             padding: 2em;
             height: 100vh;
-            width: 200px; /* Set the initial width for the aside */
+            width: 250px; /* Set the initial width for the aside */
         }
 
         main {
@@ -58,13 +56,10 @@
 
         /* Move the last 3 list item to the righ */
         .header-list {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        padding-top:1.3em;
-
-        
-        
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            padding-top:1.3em;
         }
 
         li {
@@ -77,13 +72,7 @@
         .end-item {
         margin-left: auto;
         }
-        /*Nav Style*/
-        /* Reset default list styles */
-/* Reset default list styles */
-/* Reset default list styles */
-
-
-
+      
         @media (max-width: 767px) {
             aside {
                 display: none;
@@ -93,92 +82,44 @@
             main{
                 width:100% !important;
             }
+			.dropdown-menu-right{
+			margin-top:4em;
+			}
         }
     </style>
     <body>
-               <aside>
+              
+            <aside>
+                <!-- These file contain the side navigation item -->
+                 @include('include.sideNav')
 
-               <nav>
-                    <ul class="nav-links">
-                        <li><i class="fas fa-home"></i> Home</li>
-                        <li><i class="fas fa-file-alt"></i> Manage Document</li>
-                        <li>
-                            <i class="fas fa-users"></i> Investors<i class="fas fa-caret-down"></i>
+            </aside>
 
-
-                            <ul class="sub-links">
-                                <li></i> Investor</li>
-                                <li></i> Investor groups</li>
-                            </ul>
-                        </li>
-                        <li>
-                            <i class="fas fa-briefcase"></i> Deals<i class="fas fa-caret-down"></i>
-                            <ul class="sub-links">
-                                <li></i> All deals</li>
-                                <li></i> Pipelines</li>
-                                <li></i> Pipeline view</li>
-                            </ul>
-                        </li>
-                        <li><i class="fas fa-file-invoice"></i> Invoices</li>
-                        <li>
-                            <i class="fas fa-file-signature"></i> Proposals<i class="fas fa-caret-down"></i>
-                            <ul class="sub-links">
-                                <li></i> Proposal list</li>
-                                <li></i> Templates</li>
-                            </ul>
-                        </li>
-                        <li>
-                            <i class="fas fa-tasks"></i> Activities<i class="fas fa-caret-down"></i>
-                            <ul class="sub-links">
-                                <li></i> Calendar View</li>
-                                <li></i> Activity list</li>
-                            </ul>
-                        </li>
-                        <li>
-                            <i class="fas fa-money-bill"></i> Expenses<i class="fas fa-caret-down"></i>
-                            <ul class="sub-links">
-                                <li></i> Expenses</li>
-                                <li></i> Area of Expenses</li>
-                            </ul>
-                        </li>
-                        <li>
-                            <i class="fas fa-chart-bar"></i> Report<i class="fas fa-caret-down"></i>
-                            <ul class="sub-links">
-                                <li></i> Deals</li>
-                                <li></i> Proposal</li>
-                                <li></i> Pipeline</li>
-                            </ul>
-                        </li>
-                        <li><i class="fas fa-users-cog"></i> Users & Roles</li>
-                        <li><i class="fas fa-cog"></i> Settings</li>
-                        <li><i class="fas fa-tachometer-alt"></i> Dashboard</li>
-                    </ul>
-                </nav>
-               </aside>
-               <main>
-                    <header>
-                    <ul class="header-list">
-                    <li><i class="fas fa-bars mobile" id="mobile_bar"></i></li>
-                    <li class="end-item">
-                         <i class="fas fa-sun"></i>
-                    </li>
-                    <li><i class="fas fa-search"></i></li>
-                    <li><i class="fas fa-user mobile"></i></li>
-                </ul>
-                    </header>
-                    <section id="app">sectin part of hw</section>
+            <main>
+                <header>
+                   <!-- These file contain the navigation navigation item -->
+                     @include('include.headerNav')
+                </header>
+                 <section id="app">hhhhhhhhhhh</section>
             
-               </main>
+            </main>
     </body>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
     <script>
-        $(document).ready(function() {
+       $(document).ready(function() {
           
             // Toggle aside on mobile bar click
             $("#mobile_bar").click(function() {
                 $("aside").toggle();
             });
 
-            //
+            //toggle sub menu
+            $('.nav-links li').has('.sub-links').click(function() {
+			    $(this).toggleClass('active').siblings().removeClass('active');
+		    });
             
         })
   </script>
---
+++ resources/views/include/headerNav.blade.php
@@ -0,0 +1,19 @@
+<ul class="header-list">
+                    <li><i class="fas fa-bars mobile" id="mobile_bar"></i></li>
+                    <li class="end-item">
+                         <i class="fas fa-sun"></i>
+                    </li>
+                    <li><i class="fas fa-bell"></i></li>
+                    <!-- no arrow class in the css code is use to hide the arrow-->
+					 <li class="dropdown">
+						<a class="dropdown-toggle no-arrow" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" href="#">
+							<i class="fas fa-user"></i>
+						</a>
+						<div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
+							<a class="dropdown-item" href="#"> <i class="fas fa-user"></i> Profile </a>
+							<a class="dropdown-item" href="#"><i class="fas fa-cog"></i> Settings</a>
+							<a class="dropdown-item" href="#"><i class="fas fa-bell"></i> Notifications</a>
+							<a class="dropdown-item" href="#"><i class="fas fa-sign-out-alt"></i> Logout</a>
+						</div>
+					</li>
+                </ul>
